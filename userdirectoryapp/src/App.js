d2e3735c331aefa5975ba40327a9bce8012884da
Howard Daniels <hdaniels13tat@gmail.com>
2020-04-09 23:58:10 -0400

    pushing changes

--- userdirectoryapp/src/App.js
+++ userdirectoryapp/src/App.js
@@ -1,70 +1,71 @@
 import React from 'react';
 import logo from './logo.svg';
+import ReactDOM from 'react-dom';
 import './App.css';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+const tableData = [
+	{
+		name: 'Amancio Ortega',
+		net_worth: 62.7
+	},
+	{
+		name: 'Bernard Arnault',
+		net_worth: 76
+	},
+	{
+		name: 'Bill Gates',
+		net_worth: 96.5
+	},
+	{
+		name: 'Carlos Sim Helu',
+		net_worth: 64
+	},
+	{
+		name: 'Jeff Bezos',
+		net_worth: 131
+	},
+	{
+		name: 'Larry Ellison',
+		net_worth: 58
+	},
+	{
+		name: 'Larry Page',
+		net_worth: 50.8
+	},
+	{
+		name: 'Mark Zuckerberg',
+		net_worth: 62.3
+	},
+	{
+		name: 'Michael Bloomberg',
+		net_worth: 55.5
+	},
+	{
+		name: 'Warren Buffet',
+		net_worth: 82.5
+	}
+]
 
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          User Directory
-        </h1>
-        <table>
-  <tr>
-    <th>First Name</th>
-    <th>Last Name</th>
-    <th>Age</th>
-  </tr>
-  <tr>
-    <td>Jill</td>
-    <td>Smith</td>
-    <td>50</td>
-  </tr>
-  <tr>
-    <td>Eve</td>
-    <td>Jackson</td>
-    <td>94</td>
-  </tr>
-  <tr>
-    <td>Jonathan</td>
-    <td>Scott</td>
-    <td>24</td>
-  </tr>
-</table>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+	const App = () => (
+		<div className='text-center'>
+			<h4>A list of top 10 richest billionaires.</h4>
+			<p>
+				Click on the icon next to "Net Worth" to see the sorting functionality
+			</p>
+	
+			<Table data={tableData} />
+	
+			<small>
+				* Data gathered from{' '}
+				<a
+					href='https://www.theweek.co.uk/people/57553/top-billionaires-who-richest-person-world'
+					target='_blank'>
+					theweek.co.uk
+				</a>
+			</small>
+		</div>
+	);
+	
+	ReactDOM.render(<App />, document.getElementById('app'));
 
 export default App;
