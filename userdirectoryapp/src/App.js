35847d7c2970a496654d080b059c920908c19154
Howard Daniels <hdaniels13tat@gmail.com>
2020-04-10 00:29:31 -0400

    pushing changes

--- userdirectoryapp/src/App.js
+++ userdirectoryapp/src/App.js
@@ -1,50 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import ReactDOM from 'react-dom';
+import Table from '../public/index.html';
 import './App.css';
 
-const tableData = [
-	{
-		name: 'Amancio Ortega',
-		net_worth: 62.7
-	},
-	{
-		name: 'Bernard Arnault',
-		net_worth: 76
-	},
-	{
-		name: 'Bill Gates',
-		net_worth: 96.5
-	},
-	{
-		name: 'Carlos Sim Helu',
-		net_worth: 64
-	},
-	{
-		name: 'Jeff Bezos',
-		net_worth: 131
-	},
-	{
-		name: 'Larry Ellison',
-		net_worth: 58
-	},
-	{
-		name: 'Larry Page',
-		net_worth: 50.8
-	},
-	{
-		name: 'Mark Zuckerberg',
-		net_worth: 62.3
-	},
-	{
-		name: 'Michael Bloomberg',
-		net_worth: 55.5
-	},
-	{
-		name: 'Warren Buffet',
-		net_worth: 82.5
-	}
-]
 
 	const App = () => (
 		<div className='text-center'>
