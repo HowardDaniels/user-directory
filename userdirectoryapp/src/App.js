d62d3baf47db9864f5e9e6de1833af72b235a345
Howard Daniels <hdaniels13tat@gmail.com>
2020-04-12 15:22:25 -0400

    fixed the table

--- userdirectoryapp/src/App.js
+++ userdirectoryapp/src/App.js
@@ -93,7 +93,9 @@ const UserTable = (props) => {
 export default function App() {
   return (
     <div className="App">
+
        <h1>Click on the buttons above each column to sort the table.</h1>
+      
       <UserTable
         users={[
           { id: 1, name: 'Arnold Smith', email: "asmith@developers.com", userID: 329840 },
@@ -102,14 +104,14 @@ export default function App() {
           { id: 4, name: 'Sandra Sanchez', email: "ssanchez@developers.com", userID: 193400 },
           { id: 5, name: 'Vladimir Smirnov', email: "vsmirnov@developers.com", userID: 423419 },
           { id: 6, name: 'Aisha Jefferson', email: "ajefferson@developers.com", userID: 661087 },
-		  { id: 7, name: 'Li Wong', email: "lwong@developers.com", userID: 180452 },
-		  { id: 8, name: 'Carter Jones', email: "cjones@developers.com", userID: 422984 },
-		  { id: 9, name: 'Lindsey Cohen', email: "lcohen@developers.com", userID: 904476 },
-		  { id: 10, name: 'Juan Alvarez', email: "jalvarez@developers.com", userID: 133092 },
-		  { id: 11, name: 'Miles DiSabatino', email: "mdisabatino@developers.com", userID: 102471 },
-		  { id: 12, name: 'Ashley Palmer', email: "apalmer@developers.com", userID: 455702 },
-		  { id: 13, name: 'Mohammad al-Farsi', email: "malfarsi@developers.com", userID: 448127 },
-		  { id: 14, name: 'Anna Fitzgerald', email: "afitzgerald@developers.com", userID: 231178 },
+		      { id: 7, name: 'Li Wong', email: "lwong@developers.com", userID: 180452 },
+		      { id: 8, name: 'Carter Jones', email: "cjones@developers.com", userID: 422984 },
+		      { id: 9, name: 'Lindsey Cohen', email: "lcohen@developers.com", userID: 904476 },
+		      { id: 10, name: 'Juan Alvarez', email: "jalvarez@developers.com", userID: 133092 },
+		      { id: 11, name: 'Miles DiSabatino', email: "mdisabatino@developers.com", userID: 102471 },
+		      { id: 12, name: 'Ashley Palmer', email: "apalmer@developers.com", userID: 455702 },
+		      { id: 13, name: 'Mohammad al-Farsi', email: "malfarsi@developers.com", userID: 448127 },
+		      { id: 14, name: 'Anna Fitzgerald', email: "afitzgerald@developers.com", userID: 231178 },
         ]}
       />
     </div>
